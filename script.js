3b84353c8d2b8421eaf7d907437afb9f359c2f6f
Panna Szűcs <szucspann@gmail.com>
2022-10-28 15:17:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ let waitinglist = document.querySelector(".waitinglist")
 
 let addNames = function () {
     if (inputName.value !== "") {
-        waitinglist.appendChild(document.createElement("hr"))
+        
       let member = waitinglist.appendChild(document.createElement("p"));
       member.innerText = inputName.value;
+      waitinglist.appendChild(document.createElement("hr"))
     }
     inputName.value = "";
   };
