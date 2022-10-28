59422e0c631fa5695143c421773fec40076800ee
Panna Szűcs <szucspann@gmail.com>
2022-10-28 14:47:40 +0200

    Starting js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 let names = ["Rachel", "Ross", "Monica", "Phoebe", "Chandler", "Joey", "Gunther", "Smelly Cat", "Naked Neighbor"]
+
+addNames = () => {
+    let name = document.querySelector("p")
+}
