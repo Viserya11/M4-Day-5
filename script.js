18430c728c08da3b55db8ba773786ab96069c5c0
Panna Szűcs <szucspann@gmail.com>
2022-10-30 16:09:30 +0100

    Updating

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 //let names = ["Rachel", "Ross", "Monica", "Phoebe", "Chandler", "Joey", "Gunther", "Smelly Cat", "Naked Neighbor"]
 let inputName = document.querySelector(".field")
 let waitinglist = document.querySelector(".waitinglist")
+let content = waitinglist.querySelector("p");
+let teamcards = document.querySelector(".hidingit")
 
 let addNames = function () {
     if (inputName.value !== "") {
         
       let member = waitinglist.appendChild(document.createElement("p"));
       member.innerText = inputName.value;
+      member.className = "names";
       waitinglist.appendChild(document.createElement("hr"))
     }
     inputName.value = "";
   };
 
   let resetNames = function () {
-    let content = waitinglist.querySelector("p");
-    content.innerHTML = "";
+    waitinglist.innerHTML = "";
+    teamcards.innerHTML = "";
   }
   
